44fbac5ea8f914b2ef79a631ef0379cb648be944
dannegm <im@danielgarcia.me>
2023-09-23 17:39:34 -0600

    exposing commit hash

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,10 @@ import ThemeSwitcher from '@/components/theme-switcher';
 
 export const metadata: Metadata = {
     title: 'Audio Guestbook',
+    other: {
+        'commit-hash': process.env.VERCEL_GIT_COMMIT_SHA ?? 'unknown',
+        'commit-hash-public': process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA ?? 'unknown',
+    },
 };
 
 export default function RootLayout({ children }: ChildrenContainer) {
