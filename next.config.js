b5c693eb76ec215ef00ee46996d8f72ca17caf71
dannegm <im@danielgarcia.me>
2023-09-23 17:22:16 -0600

    Adding cors policy to popups

--- next.config.js
+++ next.config.js
@@ -3,6 +3,19 @@ const nextConfig = {
     experimental: {
         serverActions: true,
     },
+    async headers() {
+        return [
+            {
+                source: '/(.*)',
+                headers: [
+                    {
+                        key: 'Cross-Origin-Opener-Policy',
+                        value: 'same-origin allow-popups',
+                    },
+                ],
+            },
+        ];
+    },
 };
 
 module.exports = nextConfig;
