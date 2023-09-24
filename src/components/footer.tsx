2167519865cb5868f72374b8d4037896903f38bf
dannegm <im@danielgarcia.me>
2023-09-24 02:14:53 -0600

    Fix error in footer again

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,5 +1,3 @@
-'use server';
-
 export default function Footer({ semver, short }: { semver: string; short: string }) {
     return (
         <footer className='fixed bottom-4 left-4 right-4 flex flex-row gap-2 justify-center text-sm text-white dark:text-white'>
