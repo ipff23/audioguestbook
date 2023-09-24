ecad724b80a028676f6bad92f4bd2e2f8230a210
dannegm <im@danielgarcia.me>
2023-09-24 02:19:12 -0600

    Fix getting commit hash

--- src/helpers/environment.ts
+++ src/helpers/environment.ts
@@ -1,7 +1,13 @@
 import { execSync } from 'child_process';
 import packageJson from '../../package.json';
 
+const VERCEL_GIT_COMMIT_SHA = process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA;
+
 export const getCommitHash = () => {
+    if (VERCEL_GIT_COMMIT_SHA !== undefined) {
+        return VERCEL_GIT_COMMIT_SHA;
+    }
+
     const localCommitHash = execSync('git rev-parse HEAD').toString().trim();
     return localCommitHash;
 };
