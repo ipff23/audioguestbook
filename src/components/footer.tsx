ccfb48b2f6dfbbb842a6f1a00e4a1b6efcdc3e43
dannegm <im@danielgarcia.me>
2023-09-24 05:54:20 -0600

    linkt to the repo

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,8 +1,12 @@
+import Link from 'next/link';
+
 export default function Footer({ semver, short }: { semver: string; short: string }) {
     return (
         <footer className='fixed bottom-4 left-4 right-4 flex flex-row gap-2 justify-center text-sm text-white dark:text-white'>
             <span className='opacity-75'>v{semver}</span>
-            <span className='opacity-50'>#{short}</span>
+            <Link href={`https://github.com/dannegm/audioguestbook/tree/${short}`}>
+                <span className='opacity-50'>#{short}</span>
+            </Link>
         </footer>
     );
 }
