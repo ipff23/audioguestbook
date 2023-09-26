6ec99c347c2f8c41d97390799d24ced86df5fe3b
dannegm <im@danielgarcia.me>
2023-09-26 15:27:57 -0600

    track player improvements

--- src/components/track-player.tsx
+++ src/components/track-player.tsx
@@ -41,11 +41,13 @@ const TrackSlider = ({
     return (
         <div className='flex-1 bg-gray-300 dark:bg-gray-800 rounded-md px-2 py-1 relative overflow-hidden'>
             <div
-                className='absolute top-0 left-0 h-full bg-sky-300 bg-sky-800 z-0'
+                className='absolute top-0 left-0 h-full bg-sky-300 dark:bg-sky-800 z-0'
                 style={{ width: `${progress}%` }}
             ></div>
             <div className='flex flex-col justify-center relative z-1'>
-                <span className='text-slate-800 dark:text-zinc-100 text-sm line-clamp-1'>{name}</span>
+                <span className='text-slate-800 dark:text-zinc-100 text-sm line-clamp-1'>
+                    {name}
+                </span>
 
                 <span className='flex flex-row items-center gap-1 text-xs'>
                     <span className='text-slate-600 dark:text-zinc-200'>
