dabdca71c6075a661bd7a4cc6ced635abccc3c01
dannegm <im@danielgarcia.me>
2023-09-29 02:18:42 -0600

    Make track list provider async

--- src/providers/providers.tsx
+++ src/providers/providers.tsx
@@ -1,8 +1,15 @@
 'use client';
+import dynamic from 'next/dynamic';
 import { NextUIProvider } from '@nextui-org/react';
 import { type ChildrenContainer } from '@/types/common';
 import BusProvider from '@/providers/bus-provider';
-import TrackListProvider from '@/providers/track-list-provider';
+// import TrackListProvider from '@/providers/track-list-provider';
+
+const TrackListProvider = dynamic(
+    // eslint-disable-next-line @typescript-eslint/promise-function-async
+    () => import('@/providers/track-list-provider'),
+    { ssr: false },
+);
 
 export function Providers({ children }: ChildrenContainer) {
     return (
