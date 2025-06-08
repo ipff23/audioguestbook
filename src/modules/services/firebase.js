e3b94d385a19fdfa3e7a1e12138b3005306bd21e
dannegm <im@danielgarcia.me>
2025-06-08 04:22:00 -0600

    Services + Env Files

---
+++ src/modules/services/firebase.js
@@ -0,0 +1,22 @@
+import { z } from 'zod';
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfigSchema = z.object({
+    apiKey: z.string(),
+    authDomain: z.string(),
+    projectId: z.string(),
+    storageBucket: z.string(),
+    messagingSenderId: z.string(),
+    appId: z.string(),
+});
+
+const firebaseConfig = firebaseConfigSchema.parse({
+    apiKey: import.meta.env.PUBLIC_FIREBASE_API_KEY,
+    authDomain: import.meta.env.PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.PUBLIC_FIREBASE_APP_ID,
+});
+
+export const app = initializeApp(firebaseConfig);
---
+++ src/modules/services/umami.js
@@ -0,0 +1,18 @@
+import { z } from 'zod';
+import umami from '@umami/node';
+
+export const shareUrl = import.meta.env.PUBLIC_UMAMI_SHARE_URL || '#';
+
+const umamiConfigSchema = z.object({
+    websiteId: z.string(),
+    hostUrl: z.string(),
+});
+
+const umamiConfig = umamiConfigSchema.parse({
+    websiteId: import.meta.env.PUBLIC_UMAMI_WEBSITE_ID,
+    hostUrl: import.meta.env.PUBLIC_UMAMI_WEBSITE_HOST_URL,
+});
+
+umami.init(umamiConfig);
+
+export { umami };
